1ed7983de2063f1a077f7d9b6a8592041b360973
Mohamma BIlal <billi12786@gmail.com>
2024-04-17 14:33:22 +0500

    extra

---
+++ lib/extra.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class pakistan extends StatefulWidget {
+  const pakistan({super.key});
+
+  @override
+  State<pakistan> createState() => _pakistanState();
+}
+
+class _pakistanState extends State<pakistan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.red,
+    );
+  }
+}
